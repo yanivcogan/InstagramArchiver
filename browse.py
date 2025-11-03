487c1207bff89ac9e50ff7951496ebd8477d14c0
yanivcogan <yanivcogan89@gmail.com>
2025-11-03 20:57:05 +0200

    disable media access limits when running locally
    to allow replayweb captures

--- browse.py
+++ browse.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 import uvicorn
+import os
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from browsing_platform.server.routes import account, post, media, media_part, archiving_session, login, search, \
     permissions, tags
 from browsing_platform.server.services.token_manager import check_token
+from dotenv import load_dotenv
 
+load_dotenv()
+is_dev = os.getenv("BROWSING_PLATFORM_DEV", "")
 app = FastAPI()
 
 app.add_middleware(
@@ -23,15 +27,20 @@ app.mount(
     StaticFiles(directory="archives"),
     name="archives",
 )
-app.mount("/thumbnails", StaticFiles(directory="thumbnails"), name="thumbnails")
+app.mount(
+    "/thumbnails",
+    StaticFiles(directory="thumbnails"),
+    name="thumbnails"
+)
 
 
 class TokenAuthMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        if request.url.path.startswith("/archives") or request.url.path.startswith("/thumbnails"):
-            token = request.query_params.get("token")
-            if not token or not check_token(token):
-                return Response("Unauthorized", status_code=401)
+        if not is_dev:
+            if request.url.path.startswith("/archives") or request.url.path.startswith("/thumbnails"):
+                token = request.query_params.get("token")
+                if not token or not check_token(token):
+                    return Response("Unauthorized", status_code=401)
         return await call_next(request)
 
 
