33d04c0a97c92404942c83dd7d790d31802fc243
yanivcogan <yanivcogan89@gmail.com>
2026-04-24 15:29:20 +0300

    clear up 401 error noise

--- browsing_platform/server/routes/permissions.py
+++ browsing_platform/server/routes/permissions.py
@@ -1,12 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi import APIRouter, Request
 
-from browsing_platform.server.services.permissions import auth_user_access, parse_token_from_header
-from browsing_platform.server.services.token_manager import check_token
+from browsing_platform.server.services.permissions import parse_token_from_header
+from browsing_platform.server.services.token_manager import check_token, TokenPermissions
 
 router = APIRouter(
     prefix="/permissions",
     tags=["permissions"],
-    dependencies=[Depends(auth_user_access)],
     responses={404: {"description": "Not found"}},
 )
 
@@ -15,8 +14,6 @@ router = APIRouter(
 async def get_permissions(request: Request):
     token = parse_token_from_header(request.headers.get("Authorization"))
     if not token:
-        raise HTTPException(status_code=401)
+        return TokenPermissions(valid=False, admin=False, user_id=None)
     token_permissions = check_token(token)
-    if not token_permissions.valid:
-        raise HTTPException(status_code=401)
     return token_permissions
